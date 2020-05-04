b070dff319437ede911d8eeff9298cdf72fa33bf
neeveermoree <askhanaev@edu.hse.ru>
2020-05-04 18:44:49 +0300

    United in one file

--- record.sh
+++ record.sh
@@ -1,4 +1,5 @@
 var=$(./list_devices.sh)
 sudo pacat --record -d $var > dump.raw
 sox -t raw -r 44100 -e signed-integer -L -b 16 -c 2 dump.raw output.wav
-rm dump.raw
+ffmpeg -i output.wav -ac 1 -ar 16000 -acodec pcm_s16le ffmpeg_output.wav
+rm dump.raw output.wav
