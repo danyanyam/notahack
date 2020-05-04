421a7fa25e4dfa792f4d54317ec056b0527e910d
Anton Khanaev <askhanaev@edu.hse.ru>
2020-05-05 00:57:18 +0300

    Use neural network from bash

--- record.sh
+++ record.sh
@@ -3,3 +3,4 @@ sudo pacat --record -d $var > dump.raw
 sox -t raw -r 44100 -e signed-integer -L -b 16 -c 2 dump.raw output.wav
 ffmpeg -i output.wav -ac 1 -ar 16000 -acodec pcm_s16le ffmpeg_output.wav
 rm dump.raw output.wav
+python model.py
